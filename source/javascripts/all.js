7d054f171a113f27b027937688f885ba4bb79bd8
Sergei Zinin <zinin@xakep.ru>
2013-12-29 01:59:53 +0800

    Initial commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,13 @@
+//= require_tree .
+
+$(document).ready(function() {
+    $('.open-popup-link').magnificPopup({
+        type:'inline',
+        midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });
+    $("table tr:nth-child(odd)").addClass("odd-row");
+    /* For cell text alignment */
+    $("table td:first-child, table th:first-child").addClass("first");
+    /* For removing the last border */
+    $("table td:last-child, table th:last-child").addClass("last");
+});
